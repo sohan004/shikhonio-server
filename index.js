188af41dd1cadbe829a040b1a1ccf31f6357e799
sohan004 <aryansohan02@gmail.com>
2023-06-12 23:37:34 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -234,8 +234,7 @@ async function run() {
             res.send({
                 clientSecret: paymentIntent.client_secret,
             });
-        });
-        // ************************************
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
